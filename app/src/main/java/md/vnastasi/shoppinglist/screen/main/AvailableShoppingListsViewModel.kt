fb941ec5e11bb2c032d20c53338f7f0bbd3ee6ad
Valentin Nastasi <pragmatiku@protonmail.com>
2023-09-10 15:35:59 +0300

    Use new viewModelFactory DSL

--- app/src/main/java/md/vnastasi/shoppinglist/screen/main/AvailableShoppingListsViewModel.kt
+++ app/src/main/java/md/vnastasi/shoppinglist/screen/main/AvailableShoppingListsViewModel.kt
@@ -1,30 +1,34 @@
 package md.vnastasi.shoppinglist.screen.main
 
 import android.util.Log
-import androidx.lifecycle.AbstractSavedStateViewModelFactory
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import md.vnastasi.shoppinglist.domain.model.ShoppingList
 import md.vnastasi.shoppinglist.domain.repository.ShoppingListRepository
-import kotlin.random.Random
 
 class AvailableShoppingListsViewModel(
     private val shoppingListRepository: ShoppingListRepository
 ) : ViewModel() {
 
-    val screenState = shoppingListRepository.getAvailableLists()
-        .map { if (it.isEmpty()) ScreenState.NoEntries else ScreenState.AvailableEntries(it) }
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(1000), ScreenState.Loading)
+    val screenState: StateFlow<ScreenState> =
+        shoppingListRepository.getAvailableLists()
+            .map { if (it.isEmpty()) ScreenState.NoEntries else ScreenState.AvailableEntries(it) }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(1000), ScreenState.Loading)
 
-    val navigationTarget: SharedFlow<NavigationTarget> = MutableSharedFlow()
+    private val _navigationTarget = MutableSharedFlow<NavigationTarget>()
+    val navigationTarget: SharedFlow<NavigationTarget> = _navigationTarget.asSharedFlow()
 
     fun onListItemDelete(shoppingList: ShoppingList) {
         Log.d("EVENTS", "Shopping list <$shoppingList> deleted")
@@ -55,10 +59,9 @@ class AvailableShoppingListsViewModel(
 
     class Factory(
         private val shoppingListRepository: ShoppingListRepository
-    ) : AbstractSavedStateViewModelFactory() {
-
-        @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle): T =
-            AvailableShoppingListsViewModel(shoppingListRepository) as T
-    }
+    ) : ViewModelProvider.Factory by viewModelFactory({
+        initializer {
+            AvailableShoppingListsViewModel(shoppingListRepository)
+        }
+    })
 }
