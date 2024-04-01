4a966e7a68a71a8f24bcf957aeca7759b010617c
Valentin Nastasi <pragmatiku@protonmail.com>
2024-04-01 19:02:54 +0200

    Added screenshot tests for overview screen

--- screen/overview/src/test/java/md/vnastasi/shoppinglist/screen/overview/ui/ListOverviewScreenshotTest.kt
+++ screen/overview/src/test/java/md/vnastasi/shoppinglist/screen/overview/ui/ListOverviewScreenshotTest.kt
@@ -3,21 +3,28 @@ package md.vnastasi.shoppinglist.screen.overview.ui
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.android.ide.common.rendering.api.SessionParams
+import com.android.resources.NightMode
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import md.vnastasi.shoppinglist.screen.overview.nav.ListOverviewScreenNavigator
-import md.vnastasi.shoppinglist.screen.overview.vm.ListOverviewViewModelSpec
-import md.vnastasi.shoppinglist.screen.overview.model.UiEvent
+import md.vnastasi.shoppinglist.domain.TestData.createShoppingList
+import md.vnastasi.shoppinglist.screen.overview.model.NavigationTarget
 import md.vnastasi.shoppinglist.screen.overview.model.ViewState
+import md.vnastasi.shoppinglist.support.theme.AppTheme
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
 
-class ListOverviewScreenshotTest {
+@RunWith(Parameterized::class)
+class ListOverviewScreenshotTest(
+    private val screenshotName: String,
+    private val viewState: ViewState,
+    private val nightMode: NightMode
+) {
 
     @get:Rule
     val paparazzi = Paparazzi(
-        deviceConfig = DeviceConfig.PIXEL_5,
+        deviceConfig = DeviceConfig.PIXEL_6,
         showSystemUi = false,
         renderingMode = SessionParams.RenderingMode.NORMAL,
         validateAccessibility = false,
@@ -25,30 +32,95 @@ class ListOverviewScreenshotTest {
     )
 
     @Test
-    fun emptyScreen() {
-        val viewState = ViewState(
-            shoppingLists = persistentListOf(),
-            navigationTarget = null,
-            toastMessage = null
-        )
-        paparazzi.snapshot("list_overview_empty") {
-            ListOverviewScreen(
-                viewModel = StubListOverviewViewModel(viewState),
-                navigator = StubListOverviewScreenNavigator()
-            )
+    fun screenshot() {
+        paparazzi.snapshot(screenshotName) {
+            AppTheme(useDarkTheme = nightMode == NightMode.NIGHT) {
+                ListOverviewScreen(
+                    viewModel = StubListOverviewViewModel(viewState),
+                    navigator = StubListOverviewScreenNavigator()
+                )
+            }
         }
     }
 
-    private class StubListOverviewScreenNavigator : ListOverviewScreenNavigator {
+    companion object {
 
-        override fun toListDetails(shoppingListId: Long) = Unit
+        @JvmStatic
+        @Parameters
+        fun parameters(): List<Array<Any>> = listOf(
+            arguments {
+                screenshotName = "empty_list_dark"
+                viewState = ViewState(
+                    shoppingLists = persistentListOf()
+                )
+                nightMode = NightMode.NIGHT
+            },
+            arguments {
+                screenshotName = "empty_list_light"
+                viewState = ViewState(
+                    shoppingLists = persistentListOf()
+                )
+                nightMode = NightMode.NOTNIGHT
+            },
+            arguments {
+                screenshotName = "non_empty_list_dark"
+                viewState = ViewState(
+                    shoppingLists = persistentListOf(
+                        createShoppingList {
+                            id = 1L
+                            name = "Test list 1"
+                        },
+                        createShoppingList {
+                            id = 2L
+                            name = "Test list 2"
+                        }
+                    )
+                )
+                nightMode = NightMode.NIGHT
+            },
+            arguments {
+                screenshotName = "non_empty_list_light"
+                viewState = ViewState(
+                    shoppingLists = persistentListOf(
+                        createShoppingList {
+                            id = 1L
+                            name = "Test list 1"
+                        },
+                        createShoppingList {
+                            id = 2L
+                            name = "Test list 2"
+                        }
+                    )
+                )
+                nightMode = NightMode.NOTNIGHT
+            },
+            arguments {
+                screenshotName = "form_dark"
+                viewState = ViewState(
+                    shoppingLists = persistentListOf(),
+                    navigationTarget = NavigationTarget.ShoppingListForm
+                )
+                nightMode = NightMode.NIGHT
+            },
+            arguments {
+                screenshotName = "form_light"
+                viewState = ViewState(
+                    shoppingLists = persistentListOf(),
+                    navigationTarget = NavigationTarget.ShoppingListForm
+                )
+                nightMode = NightMode.NOTNIGHT
+            }
+        )
     }
+}
 
-    private class StubListOverviewViewModel(private val viewState: ViewState) : ListOverviewViewModelSpec {
+private fun arguments(block: TestArguments.() -> Unit) = TestArguments().apply(block).toArray()
 
-        override val screenState: StateFlow<ViewState>
-            get() = MutableStateFlow(viewState)
+private class TestArguments {
 
-        override fun onUiEvent(uiEvent: UiEvent) = Unit
-    }
+    lateinit var screenshotName: String
+    lateinit var viewState: ViewState
+    lateinit var nightMode: NightMode
+
+    fun toArray(): Array<Any> = arrayOf(screenshotName, viewState, nightMode)
 }
