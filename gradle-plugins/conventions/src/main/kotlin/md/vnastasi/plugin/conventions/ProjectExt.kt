911c1af588d0878ce7af989d7a96e4ddd1d0e5ba
Valentin Nastasi <pragmatiku@protonmail.com>
2024-04-01 17:14:34 +0200

    Move all configuration to convention plugin

--- gradle-plugins/conventions/src/main/kotlin/md/vnastasi/plugin/conventions/ProjectExt.kt
+++ gradle-plugins/conventions/src/main/kotlin/md/vnastasi/plugin/conventions/ProjectExt.kt
@@ -5,17 +5,11 @@ import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.project
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-internal val Project.libs: VersionCatalog
-    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 internal fun Project.configureKotlin() {
     tasks.withType<KotlinCompile>().configureEach {
@@ -45,11 +39,7 @@ internal fun CommonExtension<*, *, *, *, *, *>.configureUnitTests() {
         unitTests {
             isIncludeAndroidResources = true
             isReturnDefaultValues = true
-            all {
-                it.useJUnitPlatform {
-                    includeEngines = setOf("junit-jupiter", "junit-vintage")
-                }
-            }
+            all { it.useJUnitPlatform() }
         }
     }
 }
@@ -75,7 +65,6 @@ internal fun Project.addUnitTestDependencies() {
         add("testImplementation", libs.findLibrary("mockito-kotlin").get())
         add("testImplementation", libs.findLibrary("turbine").get())
         add("testRuntimeOnly", libs.findLibrary("junit-jupiter-engine").get())
-        add("testRuntimeOnly", libs.findLibrary("junit-vintage-engine").get())
     }
 }
 
--- screen/overview/build.gradle.kts
+++ screen/overview/build.gradle.kts
@@ -1,9 +1,8 @@
-import app.cash.paparazzi.gradle.PaparazziPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("compose-screen-library.conventions")
-    alias(libs.plugins.paparazzi)
+    id("screenshot-testable-library.conventions")
 }
 
 android {
@@ -17,21 +16,5 @@ tasks.withType<KotlinCompile>().configureEach {
             "-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
             "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
         )
-    }
-}
-
-plugins.withId(libs.plugins.paparazzi.get().pluginId) {
-    afterEvaluate {
-        dependencies.constraints {
-            add("testImplementation", "com.google.guava:guava") {
-                attributes {
-                    attribute(
-                        TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
-                        objects.named<TargetJvmEnvironment>(TargetJvmEnvironment.STANDARD_JVM)
-                    )
-                }
-                because("LayoutLib and sdk-common depend on Guava's -jre published variant. See https://github.com/cashapp/paparazzi/issues/906.")
-            }
-        }
     }
 }
