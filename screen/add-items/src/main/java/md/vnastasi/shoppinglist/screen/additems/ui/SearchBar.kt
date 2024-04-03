61f63c09088cd46136bc7f3ff6364368f8beeceb
Valentin Nastasi <pragmatiku@protonmail.com>
2024-04-03 16:03:57 +0200

    Autofocus searchbar

--- screen/add-items/src/main/java/md/vnastasi/shoppinglist/screen/additems/ui/SearchBar.kt
+++ screen/add-items/src/main/java/md/vnastasi/shoppinglist/screen/additems/ui/SearchBar.kt
@@ -11,10 +11,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusTarget
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
@@ -30,9 +34,10 @@ internal fun SearchBar(
 ) {
 
     val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
 
     OutlinedTextField(
-        modifier = modifier,
+        modifier = modifier.focusRequester(focusRequester),
         value = searchTerm.value,
         placeholder = {
             Text(text = stringResource(R.string.add_items_search_title))
@@ -69,6 +74,10 @@ internal fun SearchBar(
             }
         ),
     )
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
 
 @Preview(
