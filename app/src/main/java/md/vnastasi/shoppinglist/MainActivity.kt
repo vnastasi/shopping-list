8f45e8f419f67b2246a0a593dbb33141ca1b9403
Valentin Nastasi <pragmatiku@protonmail.com>
2024-05-11 17:11:47 +0200

    Fix edge to edge (#73)
    * Use enableEdgeToEdge
    
    * Updated paddings
    
    * Updated screenshot images
    
    * Fixed unused import

--- app/src/main/java/md/vnastasi/shoppinglist/MainActivity.kt
+++ app/src/main/java/md/vnastasi/shoppinglist/MainActivity.kt
@@ -1,14 +1,21 @@
 package md.vnastasi.shoppinglist
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import md.vnastasi.shoppinglist.nav.NavigationGraph
 import md.vnastasi.shoppinglist.support.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
