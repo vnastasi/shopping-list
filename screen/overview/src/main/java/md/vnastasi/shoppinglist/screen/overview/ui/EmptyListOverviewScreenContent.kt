76691e5679a8ce1d8c747211c741ddc25a4cabb7
Valentin Nastasi <pragmatiku@protonmail.com>
2024-07-27 14:33:56 +0200

    Added Lottie animation for overview screen

--- screen/overview/src/main/java/md/vnastasi/shoppinglist/screen/overview/ui/EmptyListOverviewScreenContent.kt
+++ screen/overview/src/main/java/md/vnastasi/shoppinglist/screen/overview/ui/EmptyListOverviewScreenContent.kt
@@ -1,15 +1,23 @@
 package md.vnastasi.shoppinglist.screen.overview.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import md.vnastasi.shoppinglist.res.R
 import md.vnastasi.shoppinglist.support.theme.AppDimensions
 import md.vnastasi.shoppinglist.support.theme.AppTheme
@@ -28,10 +36,25 @@ internal fun EmptyListOverviewScreenContent(
             ),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = stringResource(R.string.overview_empty_list),
-            style = AppTypography.headlineMedium
-        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize()
+        ) {
+            val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.lottie_anim_shopping_cart))
+
+            LottieAnimation(
+                composition = composition,
+                iterations = LottieConstants.IterateForever
+            )
+
+            Text(
+                modifier = Modifier.padding(top = AppDimensions.paddingLarge),
+                text = stringResource(R.string.overview_empty_list),
+                style = AppTypography.headlineMedium
+            )
+        }
     }
 }
 
