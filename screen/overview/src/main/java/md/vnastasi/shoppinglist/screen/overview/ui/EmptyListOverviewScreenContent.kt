19f36ccf3d4b375fbe90a574a88f37db286e2ea7
Valentin Nastasi <pragmatiku@protonmail.com>
2024-07-29 06:25:42 +0300

    Added Lottie animation for overview screen

--- screen/overview/src/main/java/md/vnastasi/shoppinglist/screen/overview/ui/EmptyListOverviewScreenContent.kt
+++ screen/overview/src/main/java/md/vnastasi/shoppinglist/screen/overview/ui/EmptyListOverviewScreenContent.kt
@@ -1,22 +1,24 @@
 package md.vnastasi.shoppinglist.screen.overview.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.window.core.layout.WindowHeightSizeClass
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import md.vnastasi.shoppinglist.res.R
 import md.vnastasi.shoppinglist.support.theme.AppDimensions
@@ -27,34 +29,78 @@ import md.vnastasi.shoppinglist.support.theme.AppTypography
 internal fun EmptyListOverviewScreenContent(
     contentPaddings: PaddingValues
 ) {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass.windowHeightSizeClass
+
+    if (windowSizeClass == WindowHeightSizeClass.COMPACT) {
+        CompactContent(contentPaddings)
+    } else {
+        ExtendedContent(contentPaddings)
+    }
+}
+
+@Composable
+private fun CompactContent(contentPaddings: PaddingValues) {
     Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                bottom = contentPaddings.calculateBottomPadding()
+            )
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(
+                    start = AppDimensions.paddingMedium,
+                    end = AppDimensions.paddingMedium
+                )
+                .align(Alignment.Center),
+            text = stringResource(R.string.overview_empty_list),
+            style = AppTypography.headlineMedium
+        )
+    }
+}
+
+@Composable
+private fun ExtendedContent(contentPaddings: PaddingValues) {
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(
                 top = contentPaddings.calculateTopPadding(),
                 bottom = contentPaddings.calculateBottomPadding()
-            ),
-        contentAlignment = Alignment.Center
+            )
     ) {
-
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentSize()
+                .weight(0.75f)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center
         ) {
             val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.lottie_anim_shopping_cart))
 
             LottieAnimation(
+                modifier = Modifier.fillMaxWidth(),
                 composition = composition,
-                iterations = LottieConstants.IterateForever
+                iterations = 1
             )
 
             Text(
-                modifier = Modifier.padding(top = AppDimensions.paddingLarge),
+                modifier = Modifier
+                    .padding(
+                        top = AppDimensions.paddingLarge,
+                        start = AppDimensions.paddingMedium,
+                        end = AppDimensions.paddingMedium
+                    )
+                    .align(Alignment.CenterHorizontally),
                 text = stringResource(R.string.overview_empty_list),
                 style = AppTypography.headlineMedium
             )
+
         }
+
+        Spacer(
+            modifier = Modifier.weight(0.25f)
+        )
     }
 }
 
