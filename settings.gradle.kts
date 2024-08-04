e274622e60929d9ce099732ecc613d0664753891
Valentin Nastasi <pragmatiku@protonmail.com>
2024-08-04 12:54:23 +0200

    Delete obsolete modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,10 +25,8 @@ rootProject.name = "Shopping list"
 
 include(
     ":database:implementation",
-    ":database:test-data",
     ":domain:api",
     ":domain:implementation",
-    ":domain:test-data",
     ":support:async",
     ":support:async-unit-test",
     ":support:lifecycle",
