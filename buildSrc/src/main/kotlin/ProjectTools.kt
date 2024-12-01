b607f592c5ea4f19388cd4470e338a747d3e598f
Valentin Nastasi <pragmatiku@protonmail.com>
2024-12-01 18:37:51 +0100

    Remove 'buildSrc'

--- buildSrc/src/main/kotlin/ProjectTools.kt
+++
@@ -1,5 +0,0 @@
-import md.vnastasi.buildtools.RoomSchemaArgProvider
-import org.gradle.process.CommandLineArgumentProvider
-import java.io.File
-
-fun roomSchemaDir(schemaDir: File): CommandLineArgumentProvider = RoomSchemaArgProvider(schemaDir)
