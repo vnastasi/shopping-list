f991992fd95044623bdd2c3d292b08a1e8882a63
Valentin Nastasi <pragmatiku@protonmail.com>
2025-06-11 17:40:05 +0300

    Add delete option

--- screen/list-details/src/main/java/md/vnastasi/shoppinglist/screen/listdetails/model/UiEvent.kt
+++ screen/list-details/src/main/java/md/vnastasi/shoppinglist/screen/listdetails/model/UiEvent.kt
@@ -5,4 +5,6 @@ import md.vnastasi.shoppinglist.domain.model.ShoppingItem
 sealed interface UiEvent {
 
     data class ShoppingItemClicked(val shoppingItem: ShoppingItem): UiEvent
+
+    data class ShoppingItemDeleted(val shoppingItem: ShoppingItem): UiEvent
 }
