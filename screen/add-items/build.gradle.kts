a87fb839d83d8866c4091c33423153a25335d4c6
Valentin Nastasi <pragmatiku@protonmail.com>
2025-06-12 20:59:56 +0300

    Apply recommendations for :screen:add-items

--- screen/add-items/build.gradle.kts
+++ screen/add-items/build.gradle.kts
@@ -1,3 +1,4 @@
+import md.vnastasi.plugin.support.libs
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -10,7 +11,62 @@ android {
 }
 
 dependencies {
-    implementation(project(":screen:shared"))
+    implementation(platform(libs.compose.bom))
+    implementation(platform(libs.kotlin.bom))
+    implementation(platform(libs.kotlinx.coroutines.bom))
+
+    api(project(":domain:api"))
+    api(project(":screen:shared"))
+
+    api(libs.compose.foudation.layout)
+    api(libs.compose.runtime)
+    api(libs.kotlinx.coroutines.core)
+
+    implementation(project(":resources"))
+    implementation(project(":support:annotation"))
+    implementation(project(":support:theme") )
+    implementation(project(":support:async") )
+
+    implementation(libs.androidx.lificycle.common)
+    implementation(libs.androidx.lificycle.runtime.compose)
+    implementation(libs.androidx.lificycle.viewmodel)
+    implementation(libs.androidx.lificycle.viewmodel.savedstate)
+    implementation(libs.compose.animations.core)
+    implementation(libs.compose.foudation)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material.icons)
+    implementation(libs.compose.preview)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.text)
+    implementation(libs.compose.ui.unit)
+    implementation(libs.koin.core)
+    implementation(libs.kotlinx.collections)
+
+    debugImplementation(libs.compose.tooling)
+
+    debugRuntimeOnly(libs.compose.test.manifest)
+
+    testImplementation(platform(libs.kotlinx.coroutines.bom))
+
+    testImplementation(project(":support:async-unit-test"))
+
+    testImplementation(testFixtures(project(":domain:api")))
+
+    testImplementation(libs.assertk)
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.kotlin.reflect)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk.core)
+    testImplementation(libs.mockk.dsl)
+    testImplementation(libs.turbine)
+
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
+
+//    testImplementation("com.android.tools.layoutlib:layoutlib-api:31.4.2")
+//    testImplementation("com.android.tools.layoutlib:layoutlib:14.0.11")
+//    testImplementation("junit:junit:4.13.2")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
