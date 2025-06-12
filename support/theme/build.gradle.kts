7325a33a2a94e25eef07a34eaf306dfab36ee66d
Valentin Nastasi <pragmatiku@protonmail.com>
2025-06-12 17:43:50 +0300

    Apply recommendations for :support:theme

--- support/theme/build.gradle.kts
+++ support/theme/build.gradle.kts
@@ -7,7 +7,14 @@ android {
 }
 
 dependencies {
-
     implementation(platform(libs.compose.bom))
+
+    api(libs.compose.runtime)
+
+    implementation(libs.compose.foudation)
+    implementation(libs.compose.graphics)
     implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.text)
+    implementation(libs.compose.ui.unit)
 }
