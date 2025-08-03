e5375daa988225f68dc2adc8436010b4cc111111
Valentin Nastasi <pragmatiku@protonmail.com>
2025-08-03 20:08:32 +0200

    Refactor: Lower code coverage threshold to 80%

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ codeCoverage {
     targetBuildType.set("debug")
     reportDirectory.set(layout.buildDirectory.dir("reports/code-coverage"))
     exclusions.add("**/*Preview*")
-    coverageThreshold.set(0.90)
+    coverageThreshold.set(0.80)
 }
 
 dependencyAnalysis {
