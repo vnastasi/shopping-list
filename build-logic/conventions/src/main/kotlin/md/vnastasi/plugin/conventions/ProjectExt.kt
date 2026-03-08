0402c23de302c395f8464cb6ac89154bdf4f895c
Valentin Nastasi <pragmatiku@protonmail.com>
2026-03-08 18:21:44 +0100

    Build: Optimize JVM and GC settings for Gradle and unit tests
    - Update `org.gradle.jvmargs` in `gradle.properties` to use the G1 garbage collector and remove `MaxMetaspaceSize` and `UseParallelGC`.
    - Increase `NewSize` to `2048g` in the test JVM arguments within `ProjectExt.kt`.

--- build-logic/conventions/src/main/kotlin/md/vnastasi/plugin/conventions/ProjectExt.kt
+++ build-logic/conventions/src/main/kotlin/md/vnastasi/plugin/conventions/ProjectExt.kt
@@ -44,7 +44,7 @@ internal fun CommonExtension.configureUnitTests() {
             isIncludeAndroidResources = true
             isReturnDefaultValues = true
             all { test ->
-                test.jvmArgs("-Xmx4096m")
+                test.jvmArgs("-Xmx4096m", "-XX:NewSize=2048g")
                 test.useJUnitPlatform()
                 test.testLogging {
                     exceptionFormat = TestExceptionFormat.FULL
