ced17b8d3cfcbea2e59bbb2183123f652b57fec7
Valentin Nastasi <pragmatiku@protonmail.com>
2026-03-20 15:26:05 +0100

    Update screenshot test references for ListDetailsScreen
    - Update reference images for `ReadyListDetailsScreen` across multiple configurations: Phone, Dark mode, Landscape, 200% font size, and Small phone.

--- app/src/androidTest/java/md/vnastasi/shoppinglist/suite/DatabaseMigrationSuite.kt
+++ app/src/androidTest/java/md/vnastasi/shoppinglist/suite/DatabaseMigrationSuite.kt
@@ -5,6 +5,7 @@ import md.vnastasi.shoppinglist.db.migration.MigrationFrom2To3Test
 import md.vnastasi.shoppinglist.db.migration.MigrationFrom3To4Test
 import md.vnastasi.shoppinglist.db.migration.MigrationFrom4To5Test
 import md.vnastasi.shoppinglist.db.migration.MigrationFrom5To6Test
+import md.vnastasi.shoppinglist.db.migration.MigrationFrom6To7Test
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
@@ -15,7 +16,8 @@ import org.junit.runners.Suite
         MigrationFrom2To3Test::class,
         MigrationFrom3To4Test::class,
         MigrationFrom4To5Test::class,
-        MigrationFrom5To6Test::class
+        MigrationFrom5To6Test::class,
+        MigrationFrom6To7Test::class,
     ]
 )
 class DatabaseMigrationSuite
