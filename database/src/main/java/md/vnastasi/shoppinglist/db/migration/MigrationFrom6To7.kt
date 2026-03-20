92a3a6f8dc1f3ac8c4d7c74deafb8ed35cdde73a
Valentin Nastasi <pragmatiku@protonmail.com>
2026-03-20 12:20:52 +0100

    Feature(db): Add automatic positioning for shopping items
    - Add `CreateAutoPositionShoppingItemTriggerCallback` to automatically calculate and set the `position` of a new shopping item upon insertion.
    - Create `MigrationFrom6To7` to add a `position` column to the `shopping_items` table and initialize it with existing ID values.
    - Register the new migration and the database callback in `DatabaseModule`.

---
+++ database/src/main/java/md/vnastasi/shoppinglist/db/migration/MigrationFrom6To7.kt
@@ -0,0 +1,21 @@
+package md.vnastasi.shoppinglist.db.migration
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import md.vnastasi.shoppinglist.db.callback.CreateAutoPositionShoppingListTriggerCallback
+
+class MigrationFrom6To7 : Migration(startVersion = 5, endVersion = 6) {
+
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(ALTER_TABLE)
+        db.execSQL(UPDATE_POSITIONS)
+        db.execSQL(CreateAutoPositionShoppingListTriggerCallback.SQL_STATEMENT)
+    }
+
+    private companion object {
+
+        const val ALTER_TABLE = "ALTER TABLE shopping_items ADD COLUMN position INTEGER NOT NULL DEFAULT 0"
+
+        const val UPDATE_POSITIONS = "UPDATE shopping_items SET position = id"
+    }
+}
